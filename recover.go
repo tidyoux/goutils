86fabba7be3f8bb253e69e6c1069bc83567e2133
tidyoux <313247557@qq.com>
2018-08-22 11:42:07 +0800

    Add handlePanic for recover

--- recover.go
+++ recover.go
@@ -7,20 +7,26 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
-// DeferRecover recover from panic.
-func DeferRecover(tag string) {
-	if err := recover(); err != nil {
-		log.Errorf("%s, recover from: %v\n%s\n", tag, err, debug.Stack())
+// DeferRecover defer recover from panic.
+func DeferRecover(tag string, handlePanic func(error)) func() {
+	return func() {
+		if err := recover(); err != nil {
+			log.Errorf("%s, recover from: %v\n%s\n", tag, err, debug.Stack())
+			if handlePanic != nil {
+				handlePanic(fmt.Errorf("%v", err))
+			}
+		}
 	}
 }
 
 // WithRecover recover from panic.
-func WithRecover(tag string, f func()) {
-	defer DeferRecover(tag)
+func WithRecover(tag string, f func(), handlePanic func(error)) {
+	defer DeferRecover(tag, handlePanic)()
+
 	f()
 }
 
 // Go is a wrapper of goruntine with recover.
-func Go(name string, f func()) {
-	go WithRecover(fmt.Sprintf("goroutine %s", name), f)
+func Go(name string, f func(), handlePanic func(error)) {
+	go WithRecover(fmt.Sprintf("goroutine %s", name), f, handlePanic)
 }
