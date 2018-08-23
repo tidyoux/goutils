085a25959640c9b19eed1af9f7b9e840db77c79f
tidyoux <313247557@qq.com>
2018-08-23 16:28:54 +0800

    Add UpdateMonitor

--- time.go
+++ time.go
@@ -25,3 +25,41 @@ func WithLogTimeCost(tag string, f func()) {
 	defer DeferLogTimeCost(tag)()
 	f()
 }
+
+// UpdateMonitor monitors for update interval.
+type UpdateMonitor struct {
+	maxInterval    time.Duration
+	value          int64
+	lastUpdateTime time.Time
+	handleTimeout  func(int64, time.Duration)
+}
+
+// NewUpdateMonitor returns a new UpdateMonitor.
+func NewUpdateMonitor(maxInterval time.Duration, handleTimeout func(int64, time.Duration)) *UpdateMonitor {
+	return &UpdateMonitor{
+		maxInterval:    maxInterval,
+		value:          0,
+		lastUpdateTime: time.Now(),
+		handleTimeout:  handleTimeout,
+	}
+}
+
+// Update updates monitor state.
+func (m *UpdateMonitor) Update(value int64) {
+	if value != m.value {
+		m.value = value
+		m.lastUpdateTime = time.Now()
+	} else {
+		interval := time.Now().Sub(m.lastUpdateTime)
+		if interval > m.maxInterval {
+			if m.handleTimeout != nil {
+				m.handleTimeout(m.value, interval)
+			}
+		}
+	}
+}
+
+// Value returns the value of UpdateMoniter.
+func (m *UpdateMonitor) Value() int64 {
+	return m.value
+}
