54faf4eb731ffa80bae7f5415679195e74a78440
tidyoux <313247557@qq.com>
2018-10-11 19:24:08 +0800

    Add TryWithInterval

--- try.go
+++ try.go
@@ -1,11 +1,22 @@
 package goutils
 
+import (
+	"time"
+)
+
 // Try tries fn maxTimes.
 func Try(maxTimes int, fn func(int) error) (err error) {
+	return TryWithInterval(maxTimes, 0, fn)
+}
+
+// TryWithInterval tries fn maxTimes with interval.
+func TryWithInterval(maxTimes int, interval time.Duration, fn func(int) error) (err error) {
 	for i := 0; i < maxTimes; i++ {
 		if err = fn(i); err == nil {
 			return
 		}
+
+		time.Sleep(interval)
 	}
 	return
 }
