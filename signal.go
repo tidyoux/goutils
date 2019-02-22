d2dc68f8e9f361d26f1ba6fec1022e3a510800f1
tidyoux <tidyoux@email.com>
2019-02-22 11:53:28 +0800

    Add RegisterSignalHandler

---
+++ signal.go
@@ -0,0 +1,22 @@
+package goutils
+
+import (
+	"os"
+	"os/signal"
+
+	log "github.com/sirupsen/logrus"
+)
+
+// RegisterSignalHandler registers a global system signal handler.
+func RegisterSignalHandler(h func(os.Signal), sigs ...os.Signal) {
+	Go("signal-handler", func() {
+		c := make(chan os.Signal, 1)
+		signal.Notify(c, sigs...)
+
+		s := <-c
+		log.Warnf("receive signal: %s", s)
+		if h != nil {
+			h(s)
+		}
+	}, nil)
+}
