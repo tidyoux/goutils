496cd1102410494861ab8e6ae1d19297c4d3c785
tidyoux <tidyoux@email.com>
2019-07-24 12:26:04 +0800

    Add TraceMemStats

--- bytes.go
+++ bytes.go
@@ -1,5 +1,9 @@
 package goutils
 
+import (
+	"fmt"
+)
+
 // ReverseBytes reverse byte slice.
 func ReverseBytes(data []byte) []byte {
 	n := len(data)
@@ -12,4 +16,30 @@ func ReverseBytes(data []byte) []byte {
 		temp[n-i-1] = d
 	}
 	return temp
+}
+
+func ByteCountDecimal(b int64) string {
+	const unit = 1000
+	if b < unit {
+		return fmt.Sprintf("%d B", b)
+	}
+	div, exp := int64(unit), 0
+	for n := b / unit; n >= unit; n /= unit {
+		div *= unit
+		exp++
+	}
+	return fmt.Sprintf("%.1f %cB", float64(b)/float64(div), "kMGTPE"[exp])
+}
+
+func ByteCountBinary(b int64) string {
+	const unit = 1024
+	if b < unit {
+		return fmt.Sprintf("%d B", b)
+	}
+	div, exp := int64(unit), 0
+	for n := b / unit; n >= unit; n /= unit {
+		div *= unit
+		exp++
+	}
+	return fmt.Sprintf("%.1f %ciB", float64(b)/float64(div), "KMGTPE"[exp])
 }
---
+++ debug.go
@@ -0,0 +1,13 @@
+package goutils
+
+import (
+	"fmt"
+	"runtime"
+)
+
+func TraceMemStats() {
+	var ms runtime.MemStats
+	runtime.ReadMemStats(&ms)
+	fmt.Printf("Alloc: %s, HeapIdle: %s, HeapReleased: %s\n",
+		ByteCountBinary(int64(ms.Alloc)), ByteCountBinary(int64(ms.HeapIdle)), ByteCountBinary(int64(ms.HeapReleased)))
+}
